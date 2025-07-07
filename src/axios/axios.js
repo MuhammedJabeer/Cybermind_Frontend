95b84e01f3dfd115403bb2246480fe001ff2d290
MuhammedJabeer <jabeerjabeer1111@gmail.com>
2025-07-07 13:36:05 +0530

    second

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3999/',
+  baseURL: 'https://cybermind-backend-cg2p.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   }
